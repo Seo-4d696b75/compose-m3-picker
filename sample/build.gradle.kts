c1099ba547c6eae5751e0bd713715a5ed5f60c0e
Seo4d696b75 <s.kaoru509@gmail.com>
2025-01-15 23:06:35 +0900

    chore: sample dependency

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.material3.picker)
+    implementation(project(":lib"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
